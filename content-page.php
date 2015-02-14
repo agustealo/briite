2cb5c27ea683c8705ca33c4954b2a5932a672ed9
Agustealo Johnson <agustealo@gmail.com>
2015-02-14 09:40:57 -0500

    clean up folder

--- content-page.php
+++ content-page.php
@@ -6,10 +6,9 @@
  */
 ?>
 
+       <div class="work">
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -24,3 +23,4 @@
 		<?php edit_post_link( __( 'Edit', 'kriate' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
+</div>
--- page.php
+++ page.php
@@ -11,10 +11,7 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+Page
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
@@ -27,7 +24,4 @@ get_header(); ?>
 				?>
 
 			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
 <?php get_footer(); ?>
