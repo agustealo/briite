59f48516c5b29565faa2644b2a8bcf2b0fa4e63c
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:56:58 -0400

    Normalize Jetpack compatibility support

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -1,19 +1,22 @@
 <?php
 /**
- * Jetpack Compatibility File
- * See: http://jetpack.me/
+ * Jetpack compatibility helpers.
  *
  * @package kriate
  */
 
 /**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
+ * Add theme support for Jetpack Infinite Scroll.
+ *
+ * @return void
  */
 function kriate_jetpack_setup() {
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'footer'    => 'page',
-	) );
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'main',
+			'footer'    => 'page',
+		)
+	);
 }
 add_action( 'after_setup_theme', 'kriate_jetpack_setup' );
