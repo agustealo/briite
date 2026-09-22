0263bc45fbed99e80170c31b308460f8140652f3
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:56:06 -0400

    Harden empty-results template output

--- content-none.php
+++ content-none.php
@@ -2,30 +2,36 @@
 /**
  * The template part for displaying a message that posts cannot be found.
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package kriate
  */
-?>
 
+?>
 <section class="work no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'kriate' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'kriate' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'kriate' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: URL to create a new post. */
+					wp_kses_post( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'kriate' ) ),
+					esc_url( admin_url( 'post-new.php' ) )
+				);
+				?>
+			</p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'kriate' ); ?></p>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'kriate' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'kriate' ); ?></p>
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'kriate' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
