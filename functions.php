4e2f0d56e2d07f785f01c0d45a42825fed70371d
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:39:24 -0400

    Document historical thumbnail callback exception

--- functions.php
+++ functions.php
@@ -158,6 +158,7 @@ function kriate_render_thumbnail_column( $column_name, $post_id ) {
  * @param int    $post_id     Current post ID.
  * @return void
  */
+// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- Public callback retained for backwards compatibility.
 function fb_AddThumbValue( $column_name, $post_id ) {
 	kriate_render_thumbnail_column( $column_name, $post_id );
 }
