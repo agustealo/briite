7b34ee58efd47082c7d8f546bdc2e4fcec164d47
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:58:52 -0400

    Modernize custom header compatibility code

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -1,120 +1,112 @@
 <?php
 /**
- * Sample implementation of the Custom Header feature
- * http://codex.wordpress.org/Custom_Headers
- *
- * You can add an optional custom header image to header.php like so ...
+ * Custom Header feature integration.
  *
  * @package kriate
  */
 
 /**
- * Setup the WordPress core custom header feature.
+ * Set up the WordPress core custom header feature.
  *
- * @uses kriate_header_style()
- * @uses kriate_admin_header_style()
- * @uses kriate_admin_header_image()
+ * @return void
  */
 function kriate_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'kriate_custom_header_args', array(
-		'random-default'         => false,
-		'width'                  => 162,
-		'height'                 => 21,
-		'flex-height'            => false,
-		'flex-width'             => false,
-		'default-image'			 => get_template_directory_uri() . '/images/logo.png',
-		'default-text-color'     => '000',
-		'header-text'            => true,
-		'uploads'                => true,
-	) ) );
+	add_theme_support(
+		'custom-header',
+		apply_filters(
+			'kriate_custom_header_args',
+			array(
+				'random-default'     => false,
+				'width'              => 162,
+				'height'             => 21,
+				'flex-height'        => false,
+				'flex-width'         => false,
+				'default-image'      => get_template_directory_uri() . '/images/logo.png',
+				'default-text-color' => '000',
+				'header-text'        => true,
+				'uploads'            => true,
+			)
+		)
+	);
 }
 add_action( 'after_setup_theme', 'kriate_custom_header_setup' );
 
 if ( ! function_exists( 'kriate_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see kriate_custom_header_setup().
- */
-function kriate_header_style() {
-	$header_text_color = get_header_textcolor();
+	/**
+	 * Output custom header text styles.
+	 *
+	 * @return void
+	 */
+	function kriate_header_style() {
+		$header_text_color = get_header_textcolor();
+		$custom_header     = get_theme_support( 'custom-header' );
+		$default_color     = '000';
 
-	// If no custom options for text are set, let's bail
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value
-	if ( HEADER_TEXTCOLOR == $header_text_color ) {
-		return;
+		if ( isset( $custom_header[0]['default-text-color'] ) ) {
+			$default_color = (string) $custom_header[0]['default-text-color'];
+		}
+
+		if ( $default_color === $header_text_color ) {
+			return;
+		}
+		?>
+		<style type="text/css">
+		<?php if ( 'blank' === $header_text_color ) : ?>
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		<?php else : ?>
+			.site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?>
+		</style>
+		<?php
 	}
-
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( 'blank' == $header_text_color ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo $header_text_color; ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif; // kriate_header_style
+endif;
 
 if ( ! function_exists( 'kriate_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * @see kriate_custom_header_setup().
- */
-function kriate_admin_header_style() {
-?>
-	<style type="text/css">
-		.appearance_page_custom-header #headimg {
-			border: none;
-		}
-		#headimg h1,
-		#desc {
-		}
-		#headimg h1 {
-		}
-		#headimg h1 a {
-		}
-		#desc {
-		}
-		#headimg img {
-		}
-	</style>
-<?php
-}
-endif; // kriate_admin_header_style
+	/**
+	 * Output styles for the legacy Appearance > Header preview.
+	 *
+	 * @return void
+	 */
+	function kriate_admin_header_style() {
+		?>
+		<style type="text/css">
+			.appearance_page_custom-header #headimg {
+				border: none;
+			}
+		</style>
+		<?php
+	}
+endif;
 
 if ( ! function_exists( 'kriate_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * @see kriate_custom_header_setup().
- */
-function kriate_admin_header_image() {
-	$style = sprintf( ' style="color:#%s;"', get_header_textcolor() );
-?>
-	<div id="headimg">
-		<h1 class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div class="displaying-header-text" id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-	</div>
-<?php
-}
-endif; // kriate_admin_header_image
+	/**
+	 * Render the legacy Appearance > Header preview markup.
+	 *
+	 * @return void
+	 */
+	function kriate_admin_header_image() {
+		$header_text_color = get_header_textcolor();
+		?>
+		<div id="headimg">
+			<h1 class="displaying-header-text">
+				<a id="name" style="color:#<?php echo esc_attr( $header_text_color ); ?>;" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php echo esc_html( get_bloginfo( 'name', 'display' ) ); ?>
+				</a>
+			</h1>
+			<div class="displaying-header-text" id="desc" style="color:#<?php echo esc_attr( $header_text_color ); ?>;">
+				<?php echo esc_html( get_bloginfo( 'description', 'display' ) ); ?>
+			</div>
+			<?php if ( get_header_image() ) : ?>
+				<img src="<?php echo esc_url( get_header_image() ); ?>" alt="">
+			<?php endif; ?>
+		</div>
+		<?php
+	}
+endif;
