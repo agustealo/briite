0bd2843a792bcc40ea332bfd88fc7ae698301ec1
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 12:26:19 -0400

    Harden legacy template output and remove obsolete globals

--- content-none.php
+++ content-none.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The template part for displaying a message that posts cannot be found.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Template part for displaying a message when posts cannot be found.
  *
  * @package kriate
  */
@@ -10,24 +8,28 @@
 
 <section class="work no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'kriate' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'kriate' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'kriate' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
+			<p>
+				<?php
+				/* translators: %1$s: URL to create a new post. */
+				echo wp_kses_post(
+					sprintf(
+						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'kriate' ),
+						esc_url( admin_url( 'post-new.php' ) )
+					)
+				);
+				?>
+			</p>
 		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'kriate' ); ?></p>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'kriate' ); ?></p>
 			<?php get_search_form(); ?>
-
 		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'kriate' ); ?></p>
+			<p><?php esc_html_e( 'It seems we can’t find what you’re looking for. Perhaps searching can help.', 'kriate' ); ?></p>
 			<?php get_search_form(); ?>
-
 		<?php endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
