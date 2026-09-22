a66ef54f3cae363c73830fabf3f47c7d5c3be34c
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:26:54 -0400

    Fix search result markup

--- content-search.php
+++ content-search.php
@@ -2,57 +2,66 @@
 /**
  * The template part for displaying results in search pages.
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package kriate
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-
 	<div class="entry-content">
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<header class="entry-header">
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php kriate_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+			<?php if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php kriate_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'kriate' ) );
+		<footer class="entry-footer">
+			<?php if ( 'post' === get_post_type() ) : ?>
+				<?php
+				$categories_list = get_the_category_list( esc_html__( ', ', 'kriate' ) );
 				if ( $categories_list && kriate_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'kriate' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
+					?>
+					<span class="cat-links">
+						<?php
+						printf(
+							/* translators: %s: post categories. */
+							wp_kses_post( __( 'Posted in %1$s', 'kriate' ) ),
+							wp_kses_post( $categories_list )
+						);
+						?>
+					</span>
+				<?php endif; ?>
 
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'kriate' ) );
+				<?php
+				$tags_list = get_the_tag_list( '', esc_html__( ', ', 'kriate' ) );
 				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'kriate' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+					?>
+					<span class="tags-links">
+						<?php
+						printf(
+							/* translators: %s: post tags. */
+							wp_kses_post( __( 'Tagged %1$s', 'kriate' ) ),
+							wp_kses_post( $tags_list )
+						);
+						?>
+					</span>
+				<?php endif; ?>
+			<?php endif; ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'kriate' ), __( '1 Comment', 'kriate' ), __( '% Comments', 'kriate' ) ); ?></span>
-		<?php endif; ?>
+			<?php if ( ! post_password_required() && ( comments_open() || '0' !== get_comments_number() ) ) : ?>
+				<span class="comments-link">
+					<?php comments_popup_link( esc_html__( 'Leave a comment', 'kriate' ), esc_html__( '1 Comment', 'kriate' ), esc_html__( '% Comments', 'kriate' ) ); ?>
+				</span>
+			<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', 'kriate' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+			<?php edit_post_link( esc_html__( 'Edit', 'kriate' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-footer -->
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
