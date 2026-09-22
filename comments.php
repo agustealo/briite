f1e1a0640b87e3efd02c3c9612d8f09c7d515641
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:58:02 -0400

    Modernize comments template safely

--- comments.php
+++ comments.php
@@ -2,17 +2,11 @@
 /**
  * The template for displaying comments.
  *
- * The area of the page that contains both current comments
- * and the comment form.
+ * The area of the page that contains both current comments and the comment form.
  *
  * @package kriate
  */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
 if ( post_password_required() ) {
 	return;
 }
@@ -20,48 +14,60 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
-	<?php // You can start editing here -- including this comment! ?>
-
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'kriate' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			$kriate_comment_count = (int) get_comments_number();
+			$kriate_post_title    = get_the_title();
+
+			if ( 1 === $kriate_comment_count ) {
+				printf(
+					/* translators: %s: post title. */
+					wp_kses_post( __( 'One thought on &ldquo;%s&rdquo;', 'kriate' ) ),
+					'<span>' . esc_html( $kriate_post_title ) . '</span>'
+				);
+			} else {
+				printf(
+					/* translators: 1: number of comments, 2: post title. */
+					wp_kses_post( __( '%1$s thoughts on &ldquo;%2$s&rdquo;', 'kriate' ) ),
+					esc_html( number_format_i18n( $kriate_comment_count ) ),
+					'<span>' . esc_html( $kriate_post_title ) . '</span>'
+				);
+			}
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'kriate' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'kriate' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'kriate' ) ); ?></div>
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			<nav id="comment-nav-above" class="comment-navigation" role="navigation">
+				<h1 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'kriate' ); ?></h1>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'kriate' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'kriate' ) ); ?></div>
+			</nav><!-- #comment-nav-above -->
+		<?php endif; ?>
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
+			wp_list_comments(
+				array(
 					'style'      => 'ol',
 					'short_ping' => true,
-				) );
+				)
+			);
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'kriate' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'kriate' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'kriate' ) ); ?></div>
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			<nav id="comment-nav-below" class="comment-navigation" role="navigation">
+				<h1 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'kriate' ); ?></h1>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'kriate' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'kriate' ) ); ?></div>
+			</nav><!-- #comment-nav-below -->
+		<?php endif; ?>
 
-	<?php endif; // have_comments() ?>
+	<?php endif; ?>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'kriate' ); ?></p>
+	<?php if ( ! comments_open() && 0 < (int) get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'kriate' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
