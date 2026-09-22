a1cc27a85bb00c91dcb9cf6aa638322ac8a506f6
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:56:16 -0400

    Normalize page template loop

--- page.php
+++ page.php
@@ -4,16 +4,16 @@
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * and that other pages on the site may use a different template.
  *
  * @package kriate
  */
 
-get_header(); ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+get_header();
 
-				<?php get_template_part( 'content', 'page' ); ?>
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'content', 'page' );
+endwhile;
 
-			<?php endwhile; // end of the loop. ?>
-<?php get_footer(); ?>
+get_footer();
