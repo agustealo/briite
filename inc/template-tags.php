248a548a8558baaa9d4bb462379eea3fe7190a01
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:59:21 -0400

    Modernize template tag helpers

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -2,134 +2,136 @@
 /**
  * Custom template tags for this theme.
  *
- * Eventually, some of the functionality here could be replaced by core features.
- *
  * @package kriate
  */
 
 if ( ! function_exists( 'kriate_paging_nav' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- */
-function kriate_paging_nav() {
-	// Don't print empty markup if there's only one page.
-	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-		return;
+	/**
+	 * Display navigation to the next or previous set of posts when applicable.
+	 *
+	 * @return void
+	 */
+	function kriate_paging_nav() {
+		global $wp_query;
+
+		if ( $wp_query->max_num_pages < 2 ) {
+			return;
+		}
+		?>
+		<nav class="navigation paging-navigation" role="navigation">
+			<h1 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'kriate' ); ?></h1>
+			<div class="nav-links">
+				<?php if ( get_next_posts_link() ) : ?>
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'kriate' ) ); ?></div>
+				<?php endif; ?>
+
+				<?php if ( get_previous_posts_link() ) : ?>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'kriate' ) ); ?></div>
+				<?php endif; ?>
+			</div><!-- .nav-links -->
+		</nav><!-- .navigation -->
+		<?php
 	}
-	?>
-	<nav class="navigation paging-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'kriate' ); ?></h1>
-		<div class="nav-links">
-
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'kriate' ) ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'kriate' ) ); ?></div>
-			<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
 endif;
 
 if ( ! function_exists( 'kriate_post_nav' ) ) :
-/**
- * Display navigation to next/previous post when applicable.
- */
-function kriate_post_nav() {
-	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-	$next     = get_adjacent_post( false, '', false );
+	/**
+	 * Display navigation to adjacent posts when applicable.
+	 *
+	 * @return void
+	 */
+	function kriate_post_nav() {
+		$previous = is_attachment() ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+		$next     = get_adjacent_post( false, '', false );
 
-	if ( ! $next && ! $previous ) {
-		return;
+		if ( ! $next && ! $previous ) {
+			return;
+		}
+		?>
+		<nav class="navigation post-navigation" role="navigation">
+			<h1 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'kriate' ); ?></h1>
+			<div class="nav-links">
+				<?php
+				/* translators: %title: title of the previous post. */
+				$previous_link_text = _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'kriate' );
+				/* translators: %title: title of the next post. */
+				$next_link_text = _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link', 'kriate' );
+
+				previous_post_link( '<div class="nav-previous">%link</div>', $previous_link_text );
+				next_post_link( '<div class="nav-next">%link</div>', $next_link_text );
+				?>
+			</div><!-- .nav-links -->
+		</nav><!-- .navigation -->
+		<?php
 	}
-	?>
-	<nav class="navigation post-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'kriate' ); ?></h1>
-		<div class="nav-links">
-			<?php
-				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'kriate' ) );
-				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link',     'kriate' ) );
-			?>
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
 endif;
 
 if ( ! function_exists( 'kriate_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function kriate_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+	/**
+	 * Print HTML with meta information for the current post date and author.
+	 *
+	 * @return void
+	 */
+	function kriate_posted_on() {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf(
+			$time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_html( get_the_modified_date() )
+		);
+
+		$posted_on = sprintf(
+			/* translators: %s: post date link. */
+			_x( 'Posted on %s', 'post date', 'kriate' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+
+		$byline = sprintf(
+			/* translators: %s: post author link. */
+			_x( 'by %s', 'post author', 'kriate' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+
+		echo '<span class="posted-on">' . wp_kses_post( $posted_on ) . '</span><span class="byline"> ' . wp_kses_post( $byline ) . '</span>';
 	}
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', 'kriate' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		_x( 'by %s', 'post author', 'kriate' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
-
-}
 endif;
 
 /**
- * Returns true if a blog has more than 1 category.
+ * Determine whether the blog has more than one populated category.
  *
  * @return bool
  */
 function kriate_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'kriate_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
+	$category_count = get_transient( 'kriate_categories' );
 
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-		) );
-
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'kriate_categories', $all_the_cool_cats );
+	if ( false === $category_count ) {
+		$category_ids = get_categories(
+			array(
+				'fields'     => 'ids',
+				'hide_empty' => 1,
+				'number'     => 2,
+			)
+		);
+		$category_count = count( $category_ids );
+		set_transient( 'kriate_categories', $category_count );
 	}
 
-	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so kriate_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so kriate_categorized_blog should return false.
-		return false;
-	}
+	return $category_count > 1;
 }
 
 /**
- * Flush out the transients used in kriate_categorized_blog.
+ * Flush the transient used by kriate_categorized_blog().
+ *
+ * @return void
  */
 function kriate_category_transient_flusher() {
-	// Like, beat it. Dig?
 	delete_transient( 'kriate_categories' );
 }
 add_action( 'edit_category', 'kriate_category_transient_flusher' );
-add_action( 'save_post',     'kriate_category_transient_flusher' );
+add_action( 'save_post', 'kriate_category_transient_flusher' );
