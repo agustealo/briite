765eb8ef456eeaef34c25756d93c70767bbd1357
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:57:08 -0400

    Normalize Customizer compatibility code

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * kriate Theme Customizer
+ * Kriate Theme Customizer integration.
  *
  * @package kriate
  */
 
 /**
- * Add postMessage support for site title and description for the Theme Customizer.
+ * Add postMessage support for site title and description settings.
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ * @return void
  */
 function kriate_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
@@ -18,7 +19,9 @@ function kriate_customize_register( $wp_customize ) {
 add_action( 'customize_register', 'kriate_customize_register' );
 
 /**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ * Bind JavaScript handlers for asynchronous Customizer preview updates.
+ *
+ * @return void
  */
 function kriate_customize_preview_js() {
 	wp_enqueue_script( 'kriate_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
